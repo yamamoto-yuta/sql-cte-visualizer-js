c77e2eb8f7f5c64d328c50e6ca24c74d7ce91717
yamamoto-yuta <automatuX78@gmail.com>
2024-02-25 17:56:54 +0900

    ノードの形変更＆色分け実装

--- drawGraph.js
+++ drawGraph.js
@@ -1,6 +1,13 @@
 function drawGraph(dependencies) {
     let dot = 'digraph G {';
+    dot += 'node [shape="polygon", fillcolor="green", style="filled"] ';
+
     for (const [name, refs] of Object.entries(dependencies)) {
+        if (name === "main") {
+            dot += 'main [fillcolor="yellow", style="filled"]';
+        } else {
+            dot += `${name} [fillcolor="lightblue", style="filled"]`;
+        }
         refs.forEach(ref => {
             dot += `"${name}" -> "${ref}";`;
         });
